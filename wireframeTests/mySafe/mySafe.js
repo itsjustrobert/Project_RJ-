32028076792e2b83f3d0a79c282115fb1649d56f
itsjustrobert <robertlove99999@gmail.com>
2021-08-17 11:05:58 -0500

    pulling

---
+++ wireframeTests/mySafe/mySafe.js
@@ -0,0 +1,48 @@
+// // we need to staore all the accounts in this array
+// let allAccounts = [];
+// // we need to make a class for accounts
+// class Account{
+//     // the account will have a name and a number and a balance
+//     constructor(acctName,acctNum,balance){
+//         this.acctName = acctName;
+//         this.acctNum = acctNum;
+//         this.balance = balance;
+//         // push all instances to the array
+//         allAccounts.push(this);
+//     }
+//     // we need some methods
+//     deposit(){
+        
+//     }
+   
+    
+// }
+// let x = 0
+// if (x > 9){
+// console.log(x)
+// if(x == 21 - 21)
+// {
+//     console.log(x)
+//     if (x <= 1){
+//         console.log(x)
+//     }
+//     if(x == 0){
+//         console.log(x)
+//     }
+// }
+// else{
+//     console.log('the computer is stupid')
+// }}
+// if(x=x){
+// if(){
+
+// }else if(){
+
+// }else{
+
+// }
+// } else if(){
+
+// }else{
+
+// }
